6269ce2ca3bfce8449d383753b29c7511ad703f8
vivienl <vivienl@umich.edu>
2026-03-27 17:46:20 -0400

    kinematics FK and IK finished

--- src/kinematics.py
+++ src/kinematics.py
@@ -8,6 +8,8 @@ There are some functions to start with, you may need to implement a few more
 import numpy as np
 # expm is a matrix exponential function
 from scipy.linalg import expm
+from math import sin, cos
+
 
 
 def clamp(angle):
@@ -44,7 +46,20 @@ def FK_dh(dh_params, joint_angles, link):
 
     @return     a transformation matrix representing the pose of the desired link
     """
-    pass
+    H = np.identity(4, dtype=np.float64)
+    for idx, t in enumerate(joint_angles):
+        a, alpha, d, theta = dh_params[idx]
+        if idx == link: break   # CHANGE: breaks when joint index == desired link, matrix computed
+
+        if alpha == -1:
+            alpha = t
+        elif theta == -1:  # maybe use if instead of elif, unless only one of them is guaranteed to be 0
+            theta = t
+        A = get_transform_from_dh(a, alpha, d, theta)
+        H = np.matmul(H, A)
+
+    pose = get_pose_from_T(H)
+    return pose
 
 
 def get_transform_from_dh(a, alpha, d, theta):
@@ -60,7 +75,26 @@ def get_transform_from_dh(a, alpha, d, theta):
 
     @return     The 4x4 transformation matrix.
     """
-    pass
+    # same as PrairieLearn
+    Rot1 = np.array([[cos(theta), -sin(theta), 0, 0],
+                     [sin(theta), cos(theta), 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]], dtype=np.float64)
+    Trans1 = np.array([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, d],
+                       [0, 0, 0, 1]], dtype=np.float64)
+    Trans2 = np.array([[1, 0, 0, a],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0],
+                       [0, 0, 0, 1]], dtype=np.float64)
+    Rot2 = np.array([[1, 0, 0, 0],
+                     [0, cos(alpha), -sin(alpha), 0],
+                     [0, sin(alpha), cos(alpha), 0],
+                     [0, 0, 0, 1]], dtype=np.float64)
+    T = np.matmul(np.matmul(np.matmul(Rot1, Trans1), Trans2), Rot2)
+
+    return T
 
 
 def get_euler_angles_from_T(T):
@@ -133,4 +167,60 @@ def IK_geometric(dh_params, pose):
     @return     All four possible joint configurations in a numpy array 4x4 where each row is one possible joint
                 configuration
     """
+
+    d1 = 104.57                 # from t1 to t2, aka base offset
+    l1 = np.sqrt(200*200+50*50) # from t2 to t3, shoulder to elbow shortest distance
+    l2 = 200                    # from t3 to t4, elbow to wrist
+    l3 = 408.575 - 200 - 50     # from t4 to ee, center of gripper (?)
+    t_offset = np.arctan2(50, 200) # offset angle bewteen t3 and t2
+
+
+    # POTENTIAL TODO: theta1, theta3, and theta4's polarity might need to be reversed to match motor config
+
+    # IK for 5DOF 3-linked arm, with 360 rotating base and end effector
+    xc,yc,zc, final_theta = pose[0:3]
+
+    R = np.array([cos(final_theta), -sin(final_theta), 0],
+                 [sin(final_theta), cos(final_theta), 0],
+                 0, 0, 1)
+    
+    zc = zc - l3
+
+    # find base orientation
+    theta1 = np.arctan2(yc, xc)
+
+    # find wrist position
+    l1 = dh_params[1]
+
+    # find the wrist orientation
+    r_squared = xc**2 + yc**2
+    s_squared = (zc - d1)**2    # d1 is the height of base from shoulder
+
+    # elbow down solution
+    theta3 = -np.arccos((r_squared + s_squared - l1*l1 - l2*l2)/(2*l1*l2))
+    theta2 = np.arctan2(np.sqrt(s_squared), np.sqrt(r_squared)) - np.arctan2(l2*np.sin(theta3), l1 + l2*np.cos(theta3)) 
+
+    theta3 += np.pi/2 -t_offset
+    theta3 = -theta3
+    theta2 = np.pi/2 - t_offset - theta2 # offset
+
+    theta4 = final_theta- (theta2 + theta3)
+
+    if theta1 >= np.pi or theta1 <= -np.pi:
+        return False, [0, 0, 0, 0, 0]
+
+    if theta2 >= np.deg2rad(113) or theta2 <= -np.deg2rad(108):
+        return False, [0, 0, 0, 0, 0]
+
+    if theta3 >= np.deg2rad(93) or theta3 <= -np.deg2rad(108):
+        return False, [0, 0, 0, 0, 0]
+
+    if theta4 >= np.deg2rad(123) or theta4 <= -np.deg2rad(100):
+        return False, [0, 0, 0, 0, 0]
+
+    return [theta1,theta2,theta3,theta4] # ignore theta5 (end effector's orientation) 
+
+
+
+
     pass
